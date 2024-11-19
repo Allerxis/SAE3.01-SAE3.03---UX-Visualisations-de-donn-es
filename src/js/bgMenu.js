80152358708ab53909c269cafe291b4945bd292e
ANNET Romain <romain.annet.etu@univ-lemans.fr>
2024-11-19 18:00:36 +0100

    bg menu fonctionnel pour importation dans d'autre html

--- src/js/bgMenu.js
+++ src/js/bgMenu.js
@@ -1,9 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const burgerBtn = document.getElementById('burger-btn');
+    const menu = document.getElementById('menu');
 
-
-// Toggle Menu
-const burgerBtn = document.getElementById('burger-btn');
-const menu = document.getElementById('menu');
-
-burgerBtn.addEventListener('click', () => {
-    menu.classList.toggle('hidden');
+    if (burgerBtn) { // Vérifie que l'élément existe avant d'ajouter l'événement
+        burgerBtn.addEventListener('click', () => {
+            menu.classList.toggle('hidden');
+        });
+    }
 });
