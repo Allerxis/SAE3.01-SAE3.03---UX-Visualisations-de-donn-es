787f2aa0ae0b169587e66c6908e9840ae06f94cb
ANNET Romain <romain.annet.etu@univ-lemans.fr>
2024-11-22 08:29:58 +0100

    Mise à jour navBarre PC

---
+++ src/js/navBarre.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const tabs = document.querySelectorAll("nav a"); // Sélectionner tous les liens du menu
+    const currentPath = window.location.pathname; // Obtenir le chemin de l'URL actuelle
+  
+    tabs.forEach((tab) => {
+      if (tab.getAttribute("href") === currentPath) {
+        // Activer le lien correspondant à l'URL actuelle
+        tab.classList.add("text-black", "active:bg-gray-200");
+        tab.classList.remove("text-gray-500");
+      } else {
+        // Désactiver les autres liens
+        tab.classList.remove("text-black", "active:bg-gray-200");
+        tab.classList.add("text-gray-500");
+      }
+    });
+  });
+  
