4619acf5e3df37bd313272be8d8ccbb7887e3e3a
ANNET Romain <romain.annet.etu@univ-lemans.fr>
2024-12-12 09:43:18 +0100

    mise a jour bordure haute pages header

--- src/js/navBarre.js
+++ src/js/navBarre.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const tabs = document.querySelectorAll("nav a"); // Sélectionner tous les liens du menu
-  const currentPath = window.location.pathname; // Obtenir le chemin de l'URL actuelle
+  const currentPath = window.location.pathname.split('/').pop(); // Obtenir uniquement le nom du fichier (sans dossier)
 
   tabs.forEach((tab) => {
-    if (tab.getAttribute("href") === currentPath) {
+    // Récupérer le nom de fichier de l'attribut href (sans ./)
+    const tabPath = tab.getAttribute("href").replace("./", "");
+
+    if (tabPath === currentPath) {
       // Activer le lien correspondant à l'URL actuelle
       tab.classList.add("text-black", "active:bg-gray-200");
       tab.classList.remove("text-gray-500");
