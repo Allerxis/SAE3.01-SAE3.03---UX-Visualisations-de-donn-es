ac5d45390287ffceadfa0f3b7b11070634c0f7f3
Delton241 <tonydelage937@gmail.com>
2024-12-12 10:01:39 +0100

    BgMenu modifié (tony)

--- src/js/bgMenu.js
+++ src/js/bgMenu.js
@@ -7,4 +7,11 @@ document.addEventListener('DOMContentLoaded', function () {
             menu.classList.toggle('hidden');
         });
     }
+
+    function toggleMenu() {
+        const menu = document.querySelector(".menu-links");
+        const burgerBtn = document.querySelector(".hamburger-icon");
+        menu.classList.toggle("open")
+        icon.classList.toggle("open")
+    }
 });
